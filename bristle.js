4b6e6f56828bf61cfb673d4e0103e52f5f89cbf5
Caleb <caleb@Calebs-MacBook-Pro.local>
2017-10-20 01:43:54 -0600

    create typechecking and handling for rendering

--- bristle.js
+++ bristle.js
@@ -1,5 +1,34 @@
 class Bristle {
-  
+  constructor(elementType,render){
+    if (typeof elementType !=='string' || elementType.length <= 0){
+      throw this.error('Invalid Element Type!');
+    }
+    console.log(render);
+    if (typeof render === 'string' || typeof render === 'number' || typeof render === 'boolean') {
+      this.render(render);
+    } else if (typeof render === 'function') {
+      this.renderMethod = render;
+      this.renderMethod.bind(this);
+      this.value = null;
+    } else if (typeof render === 'null' || typeof render === 'undefined'){
+      this.value = undefined;
+    } else if (render instanceof Bristle){
+      this.render(render);
+      this.value = null;
+    } else {
+      throw this.error('Invalid Render Type');
+    }
+    this.type = elementType;
+    this.render = this.render.bind(this);
+  }
+  render(value){
+    if (value){
+
+    }
+  }
+  error(message){
+    return new Error('BRISTLE ERROR: '+message);
+  }
 }
 
 module.exports = Bristle;
