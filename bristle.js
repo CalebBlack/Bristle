89b05b8d72ef96cc8d57aa917bf996aeae4c9a53
Caleb <caleb@Calebs-MacBook-Pro.local>
2017-10-20 02:10:15 -0600

    finish first prototype of render and appendTo methods

--- bristle.js
+++ bristle.js
@@ -1,26 +1,49 @@
+const validRenderPrimitives = ['string','boolean','number']
 class Bristle {
   constructor(elementType,render){
+    this.value = null;
     this.render = this.render.bind(this);
+    this.appendTo = this.appendTo.bind(this);
     if (typeof elementType !=='string' || elementType.length <= 0){
       throw this.error('Invalid Element Type!');
     }
+    this.element = document.createElement(elementType);
     this.type = elementType;
-    if (typeof render === 'string' || typeof render === 'number' || typeof render === 'boolean' || render instanceof Bristle || typeof render === 'null' || typeof render === 'undefined') {
+    if (validRenderPrimitives.includes(typeof render) || render instanceof Bristle || render === null || render === undefined) {
       this.render(render);
     } else if (typeof render === 'function') {
+      this.render();
       render.bind(this)(this.render);
     } else {
       throw this.error('Invalid Render Value.');
     }
   }
   render(value){
-    if (value){
-
+    if (value !== null){
+      this.value = value;
     }
+    if (this.hasOwnProperty('parent') && validRenderPrimitives.includes(typeof this.value)){
+      this.element.innerHTML = this.value;
+    }
+  }
+  appendTo(element,rerender=true){
+    if (!element instanceof HTMLElement) {
+      throw this.error('Invalid DOM Element to Append To!');
+    }
+    this.parent = element;
+    this.parent.appendChild(this.element);
+    if (rerender === true) {
+      this.render();
+    }
+  }
+  remove(){
+
   }
   error(message){
     return new Error('BRISTLE ERROR: '+message);
   }
 }
+rawUpdate(bristle,value){
 
+}
 module.exports = Bristle;
