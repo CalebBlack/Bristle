c272990b4ae355f4876c6276c1f7a086368a1243
Caleb <caleb@Calebs-MacBook-Pro.local>
2017-10-21 17:07:02 -0600

    Create link to source

--- docs/src/footer.js
+++ docs/src/footer.js
@@ -2,6 +2,6 @@ import Bristle from 'bristlejs';
 import './css/footer.css';
 
 const footer = new Bristle('footer');
-new Bristle('span','SPA Made with Bristle and BristleRouter!',footer);
+new Bristle({type:'a',href:'https://github.com/CalebBlack/BristleJS/tree/master/docs/src'},'SPA Made with Bristle and BristleRouter! (Source)',footer);
 
 export default footer;
