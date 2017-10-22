2a0ab9b065baa0449335a8841ec71db34c910459
Caleb <caleb@Calebs-MacBook-Pro.local>
2017-10-21 22:02:24 -0600

    update documentation

--- docs/src/documentation.js
+++ docs/src/documentation.js
@@ -1,5 +1,8 @@
 import Bristle from 'bristlejs';
+import methods from './documentation/methods';
 
 var docs = new Bristle({type:'div',class:'documentation'});
-var title = new Bristle('h1','Documentation',docs);
+new Bristle({type:'h1',class:'title'},'Documentation',docs);
+methods.appendTo(docs);
+
 export default docs;
